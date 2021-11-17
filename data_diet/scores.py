9109351a7318f5d6eb3a3eca30d3533b8d658dab
Faldict <legadoubledyan@gmail.com>
2021-11-16 22:25:00 -0800

    compute fairness scores

--- data_diet/scores.py
+++ data_diet/scores.py
@@ -67,6 +67,15 @@ def get_score_fn(fn, params, state, score_type):
   return score_fn
 
 
+def get_fair_score_fn(fn, params, state, score_type):
+  if score_type == 'covariance':
+    print(f'compute {score_type}...')
+    score_fn = get_covariance_fn(fn, params, state)
+  else:
+    raise NotImplementedError
+  return score_fn
+
+
 def compute_scores(fn, params, state, X, Y, batch_sz, score_type):
   n_batches = X.shape[0] // batch_sz
   Xs, Ys = np.split(X, n_batches), np.split(Y, n_batches)
@@ -75,6 +84,18 @@ def compute_scores(fn, params, state, X, Y, batch_sz, score_type):
   for i, (X, Y) in enumerate(zip(Xs, Ys)):
     print(f'score batch {i+1} of {n_batches}')
     scores.append(score_fn(X, Y))
+  scores = np.concatenate(scores)
+  return scores
+
+
+def compute_fair_scores(fn, params, state, X, Y, Z, batch_size, score_type):
+  n_batches = X.shape(0) // batch_size
+  Xs, Ys, Zs = np.split(X, n_batches), np.split(Y, n_batches), np.split(Z, n_batches)
+  score_fn = get_score_fn(fn, params, state, score_type)
+  scores = []
+  for i, (x, y, z) in enumerate(zip(Xs, Ys, Zs)):
+    print(f'score batch {i+1} of {n_batches}')
+    scores.append(score_fn(x, y, z))
   scores = np.concatenate(scores)
   return scores
 
---
+++ scripts/run_fair_score.py
@@ -0,0 +1,37 @@
+# python get_fair_score.py <ROOT:str> <EXP:str> <RUN:int> <STEP:int> <BATCH_SIZE:int> <TYPE:str>
+
+from data_diet.data import load_data, load_fairness_dataset
+from data_diet.scores import compute_fair_scores, compute_scores
+from data_diet.utils import get_fn_params_state, load_args
+import sys
+import numpy as np
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--root', type=str, default=os.getcwd(),
+                    help='dir os root')
+parser.add_argument('--exp', type=str, default='CelebA',
+                    help='dataset')
+parser.add_argument('--run', type=int, default=1)
+parser.add_argument('--step', type=int, default=78000)
+parser.add_argument('--batch-size', type=int, default=1000)
+parser.add_argument('-t', '--type', type=str, default='covariance')
+
+args = parser.parse_args()
+
+run_dir = os.path.join(args.root, f'/exps/{args.exp}/run_{args.run}')
+arg = load_args(run_dir)
+arg.load_dir = run_dir
+arg.ckpt = args.step
+
+_, X, Y, Z, _, _, _, args = load_fairness_dataset(arg)
+fn, params, state = get_fn_params_state(arg)
+scores = compute_fair_scores(fn, params, state, X, Y, Z, args.batch_size, args.type)
+
+path_name = 'covariance_scores'
+
+save_dir = os.path.join(run_dir, path_name)
+save_path = os.path.join(run_dir, path_name, f'ckpt_{STEP}.npy')
+if not os.path.exists(save_dir): os.makedirs(save_dir)
+np.save(save_path, scores)
